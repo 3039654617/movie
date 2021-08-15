d605b26203797e9cc1c26116309404fd11fc8311
penglang <3039654617@qq.com>
2021-08-15 16:33:16 +0800

    电影实体类

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,27 @@
-console.log(123123456);
+import { plainToClass } from "class-transformer";
+import { validate } from "class-validator";
+import { Movie } from "./entity/Movie";
+
+// let obj = new Movie();
+// obj.name = "penglang";
+// obj.area="中国贵州";
+// obj.poster="";
+// obj.type= ["喜剧"];
+// obj.isComing = true;
+// obj.time = 100
+
+let plain = {
+    name : "penglang",
+    area:"中国贵州",
+    poster:"",
+    type: ["喜剧"],
+    isComing : true,
+    time : 100
+}
+
+let obj = plainToClass(Movie, plain)
+
+validate(obj).then((err) => {
+    console.log(err); 
+    
+})
