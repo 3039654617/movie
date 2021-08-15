e4119284989a13bd5083b8891ce0ceef3962f25e
penglang <3039654617@qq.com>
2021-08-15 19:31:21 +0800

    movie实体建立以及验证和将平面对象转换为movie实体类的测试

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,28 @@
-console.log(123123456);
+
+import { plainToClass } from "class-transformer";
+import { validate } from "class-validator";
+import { Movie } from "./entities/Movie";
+
+// const obj = new Movie();
+
+// obj.poster = '';
+// obj.name = '三傻大闹天空';
+// obj.areas = [""];
+// obj.types = [""];
+// obj.description = '这是电影1';
+// obj.isComing = true
+
+const obj = {
+    poster: '',
+    name: 1,
+    areas: [""],
+    types: [""],
+    description: '这是电影1',
+    isComing: true
+}
+
+let plain = plainToClass(Movie, obj)
+
+validate(plain).then((err) => {
+    console.log(err, plain);
+})
