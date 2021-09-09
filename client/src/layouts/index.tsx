f92e991acc19c464be68a9bbd2590270f686454c
penglang <3039654617@qq.com>
2021-09-09 08:41:23 +0800

    调整layout

--- client/src/layouts/index.tsx
+++ client/src/layouts/index.tsx
@@ -1,12 +1,42 @@
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import './index.less'
 import React from 'react';
-import './index.less';
 
-const BasicLayout: React.FC = props => {
+const { SubMenu } = Menu;
+const { Header, Content, Footer, Sider } = Layout;
+
+const BasicLayout: React.FC = () => {
   return (
-    <div className='normal'>
-      <h1 className='title'>Yay! Welcome to umi!</h1>
-      {props.children}
-    </div>
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
+          <Sider className="site-layout-background" width={200}>
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              style={{ height: '100%' }}
+            >
+              <SubMenu key="sub1" icon={<UserOutlined />} title="关于电影">
+                <Menu.Item key="1">电影菜单</Menu.Item>
+                <Menu.Item key="2">添加电影</Menu.Item>
+              </SubMenu>
+            </Menu>
+          </Sider>
+          <Content style={{ padding: '0 24px', minHeight: 280 }}>Content</Content>
+        </Layout>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
   );
 };
 
